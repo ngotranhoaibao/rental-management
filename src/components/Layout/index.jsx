5208d133c87dcb683e070b159173c0a9db0717af
ngotranhoaibao <ngotranhoaibao@gmail.com>
2025-11-28 16:03:39 +0700

    feat: Add Vercel configuration for URL rewrites and clean up Layout component formatting

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import Header from "../Header";
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
-import  AppSidebar  from "../AppSidebar";
+import AppSidebar from "../AppSidebar";
 const Layout = () => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-      <Header></Header>
+        <Header></Header>
 
-      <main className="flex-1 overflow-y-auto p-4 lg:p-8">
-
-        <Outlet />
-      </main>
+        <main className="flex-1 overflow-y-auto p-4 lg:p-8">
+          <Outlet />
+        </main>
       </div>
     </SidebarProvider>
   );
